472a6a2062af6458236052c6719f778a6f92402b
Kapitan-Black <ikletzko@gmail.com>
2024-08-25 19:08:24 +0300

    a

--- src/routes/HotelsUpdateRoute.ts
+++ src/routes/HotelsUpdateRoute.ts
@@ -1,14 +1,11 @@
 import express from "express";
-import { param } from "express-validator";
-import HotelsController from "../controllers/HotelsController";
 import { jwtCheck } from "../middleware/auth";
 import HotelsUpdateController from "../controllers/HotelsUpdateController";
 
 const router = express.Router();
 
-router.put("/:id", HotelsUpdateController.UpdateHotel);
+router.put("/:id", jwtCheck, HotelsUpdateController.UpdateHotel);
 
-router.delete("/", HotelsUpdateController.DeleteImageUrls)
-
+router.delete("/", jwtCheck, HotelsUpdateController.DeleteImageUrls);
 
 export default router;
